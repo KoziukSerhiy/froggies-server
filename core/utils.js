3e89e2e18812c8244edd82f11b21a1dc0219cd06
Serhiy Koziuk <seryjkozyk@gmail.com>
2024-03-26 12:39:03 +0200

    add readDataFromDB description

--- core/utils.js
+++ core/utils.js
@@ -1,15 +1,27 @@
 const fs = require("fs");
 const { DBFile } = require("./constants");
 
+/**
+ * Reads data from the database based on the provided key.
+ *
+ * @param {string} key - The key to retrieve the data from the database.
+ * @returns {*} - The data associated with the provided key, or undefined if the key is invalid or data retrieval fails.
+ */
 const readDataFromDB = (key) => {
-  if (!key) {
+  if (typeof key !== "string" || key.trim() === "") {
     return;
   }
 
-  const dbRaw = fs.readFileSync(DBFile);
-  const data = JSON.parse(dbRaw)[key];
+  try {
+    const dbRaw = fs.readFileSync(DBFile);
+    const data = JSON.parse(dbRaw)[key];
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error("Error reading data from DB:", error);
+
+    return;
+  }
 };
 
 module.exports = { readDataFromDB };
